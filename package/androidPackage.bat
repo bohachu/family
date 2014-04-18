f432550d13e2c7bb384a4ae5cb827e5cc869553b
bingyulee <bigcookie.lee@gmail.com>
2014-04-18 10:54:37 +0800

    bigcookie: 打包檔案更新

--- package/androidPackage.bat
+++ package/androidPackage.bat
@@ -1,2 +1,2 @@
-copy /Y ../Main.swf
-call "D:\Resource\AdobeAIRSDK 3.7\bin\adt" -package -target apk-captive-runtime -storetype pkcs12 -keystore ../androidFlash.p12 -storepass 123456 Family-v1.4.0418.apk Main-app-android.xml -extdir "C:/github/flashCommon/ane/Android" Main.swf icons Resource
+copy /Y ..\Main.swf
+call "D:\Resource\AdobeAIRSDK 3.7\bin\adt" -package -target apk-captive-runtime -storetype pkcs12 -keystore ../../androidFlash.p12 -storepass 123456 Family-v1.4.0418.apk Main-app-android.xml -extdir "C:/github/flashCommon/ane/Android" Main.swf icons Resource
