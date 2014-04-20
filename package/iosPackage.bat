d27869b6b3f18921c2dfda9f1bac9bf0ed170093
bingyulee <bigcookie.lee@gmail.com>
2014-04-20 11:09:22 +0800

    bigcookie: v1.4 版打�

--- package/iosPackage.bat
+++ package/iosPackage.bat
@@ -1,2 +1,2 @@
 copy /Y ..\Main.swf
-call "D:\Resource\AdobeAIRSDK 4.0 Beta\bin\adt" -package -target ipa-test -provisioning-profile ../../pan_asia2.mobileprovision -storetype pkcs12 -keystore ../../pan_asia.p12 -storepass cameo Family-v1.4.0418.ipa Main-app-ios.xml -extdir "C:/github/flashCommon/ane/iOS" Main.swf Default.png Default@2x.png Default-568h@2x.png icons Resource
+call "D:\Resource\AdobeAIRSDK 4.0 Beta\bin\adt" -package -target ipa-test -provisioning-profile ../../pan_asia2.mobileprovision -storetype pkcs12 -keystore ../../pan_asia.p12 -storepass cameo Family-v1.4.0420.ipa Main-app-ios.xml -extdir "C:/github/flashCommon/ane/iOS" Main.swf Default.png Default@2x.png Default-568h@2x.png icons Resource
