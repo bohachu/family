35593a08a1d9726fb9d85a57c629f29df64652e5
bingyulee <bigcookie.lee@gmail.com>
2014-06-20 10:19:16 +0800

    bigcookie: v1.6 版 Release

--- package/androidPackage.bat
+++ package/androidPackage.bat
@@ -1,2 +1,2 @@
 copy /Y ..\Main.swf
-call "D:\Resource\AdobeAIRSDK 3.7\bin\adt" -package -target apk-captive-runtime -storetype pkcs12 -keystore ../../androidFlash.p12 -storepass 123456 Family-v1.5.0502.apk Main-app-android.xml -extdir "C:/github/flashCommon/ane/Android" Main.swf icons Resource
+call "D:\Resource\AdobeAIRSDK 3.7\bin\adt" -package -target apk-captive-runtime -storetype pkcs12 -keystore ../../androidFlash.p12 -storepass 123456 Family-v1.6.0620.apk Main-app-android.xml -extdir "C:/github/flashCommon/ane/Android" Main.swf icons Resource
